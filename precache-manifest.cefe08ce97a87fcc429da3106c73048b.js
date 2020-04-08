87a6cdc2a8b4d51d0848d9a0a2dad8d9ca526353
Bilal Tehseen Ahmed <bilaltehseen@gmail.com>
2020-04-09 00:07:43 +0500

    Updates

---
+++ precache-manifest.cefe08ce97a87fcc429da3106c73048b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a26ea5bfffb13230aabd6cb6ccb5856",
+    "url": "/SentecCertificate/index.html"
+  },
+  {
+    "revision": "3d1f443b778f82fca6e6",
+    "url": "/SentecCertificate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8651ba2669a14834f607",
+    "url": "/SentecCertificate/static/js/2.34342c15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SentecCertificate/static/js/2.34342c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1f443b778f82fca6e6",
+    "url": "/SentecCertificate/static/js/main.cd654198.chunk.js"
+  },
+  {
+    "revision": "d66e1182498a7e26796b",
+    "url": "/SentecCertificate/static/js/runtime-main.6f92d2fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SentecCertificate/static/media/logo.5d5d9eef.svg"
+  }
+]);
